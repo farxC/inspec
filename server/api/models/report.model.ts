45e18e9e47662587c1bf7455f6a73c284ae80421
Rafael Nunes <rafaelflamengo07@outlook.com>
2024-12-23 15:39:02 -0400

    Folder structure changes

--- server/api/models/report.model.ts
+++
@@ -1,8 +0,0 @@
-//Modules
-import { OkPacketParams } from "mysql2";
-import { connection } from "../src/config/database";
-
-//Intefaces 
-import { report_data } from "../../../src/types/report_data";
-
-
--- server/api/sql/models.sql
+++
@@ -1,9 +0,0 @@
-
-
-CREATE TABLE IF NOT EXISTS OS(
-	id INT NOT NULL,
-    report_date VARCHAR(40),
-    imagePathIdentifier VARCHAR(255) NOT NULL,
-    imagePathOverview VARCHAR(255) NOT NULL,
-    imagePathFailureEvidence VARCHAR(255) NOT NULL
-);
