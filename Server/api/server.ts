f62d3021caf3094543f186fb37745a4bebd8fbd9
Rafael Nunes <rafaelflamengo07@outlook.com>
2025-01-02 16:28:36 -0400

    Added React to build the Client

--- Server/api/server.ts
+++ Server/api/server.ts
@@ -3,7 +3,7 @@ import {app} from './src/app'
 
 dotenv.config();
 
-const port = process.env.PORT ||3000;
+const port = process.env.PORT || 3100;
 
 app.listen(port, () => {
     return console.log(`Server running at http://localhost:${port}`);
